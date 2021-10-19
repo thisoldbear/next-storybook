019459de9f62cbdaf87cbc70dc1eac4f41834af9
Daniel Williams <danielwilliams.design@gmail.com>
2021-10-19 18:18:08 +0100

    Add Sass modules support (#1)
    * Remove example code
    
    * Add sass support
    
    * Add preset-scss
    
    * Refactor Button to use Sass modules

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,10 @@
-const Index = () => <h1>Hello world</h1>;
+import { Button } from "../stories/Button";
+
+const Index = () => (
+  <>
+    <h1>Hello world</h1>
+    <Button label="I'm a button from stories/Button" />
+  </>
+);
 
 export default Index;
